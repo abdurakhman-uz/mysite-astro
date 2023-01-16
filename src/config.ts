ea65d460eb95afd8dd9e480dfe64d9e6adda0767
abdurakhman-uz <rocklee667@yandex.kz>
2023-01-16 15:28:02 +0500

    update any files

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = 'Abduazimov.uz';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const GRADIENT_COLOR1 = '166, 192, 254';
+export const GRADIENT_COLOR2 = '#f68084';
